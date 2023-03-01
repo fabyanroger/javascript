22f7491f0dd0ed8248a4a0d158048dec03e73063
Fabyan Roger Oliveira Bastos <fabyanroger@hotmail.com>
2023-03-01 17:51:39 -0300

    ex012 ao 14

---
+++ aula12/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas`)
+if (hora >= 0 && hora < 6) {
+    console.log('Boa madrugada')
+} else if (hora >= 5 && hora < 12) {
+    console.log('Bom dia')
+} else if (hora >= 12 && hora < 18) {
+    console.log('Boa tarde')
+} else if (hora >= 18 && hora < 24) {
+    console.log('Boa noite')
+}
