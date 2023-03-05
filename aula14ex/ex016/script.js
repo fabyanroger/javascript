9c1f49cf78a4ed3d8878265568367d48714720c6
Fabyan Roger Oliveira Bastos <fabyanroger@hotmail.com>
2023-03-05 01:09:42 -0300

    updates js ex16-17

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,33 @@
-alert(Ólá)
+function contar() {
+    let inicio = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    let result = document.querySelector('div#res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        result.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    } else {
+        result.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //contagem crescente
+            for (let c = 1; c <= f; c += p) {
+                result.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem decrescente
+            for (let c = i; c >= f; c-= p) {
+                result.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        result.innerHTML += `\u{1f3c1}`
+    }
+}
