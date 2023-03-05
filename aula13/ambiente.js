757a7550ad632020b6322f6e90669c162ac46c1d
Fabyan Roger Oliveira Bastos <fabyanroger@hotmail.com>
2023-03-04 22:46:58 -0300

    update js

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 5) 
+
+
+
+
+
+
+/*
+var cont = 1
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+}
+*/
+
+/*
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+)*/
